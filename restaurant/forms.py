d098cc4774d5e96498ff11ead0116fa37b86338d
juliachelsie <>
2024-01-24 12:57:00 +0000

    Added attrs to table in form

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -2,14 +2,14 @@ from django.forms import ModelForm
 from django import forms
 from .models import Reservation
 
-tableChoices = {
-    'window1': 'Bigger table by window',
-    'window2': 'Smaller table by window',
-    'bigTable1': 'Big table in isle',
-    'bigTable2': 'Big table upstairs'
-}
+tableChoices = [
+    ('1', 'Bigger table by window'),
+    ('2', 'Smaller table by window'),
+    ('3', 'Big table in isle'),
+    ('4', 'small table in isle')
+]
 
-sittingTimes = [('3-5', '15-17'), ('5-7', '17-19'), ('7-9', '19-21'),]
+sittingTimes = [('3-5', '3pm-5pm'), ('5-7', '5pm-7pm'), ('7-9', '7pm-9pm'),]
 
 
 class Reserve_table_form(ModelForm):
@@ -20,12 +20,16 @@ class Reserve_table_form(ModelForm):
     phone_number = forms.IntegerField(widget=forms.TextInput(
         attrs={'placeholder': 'Phone-number', 'style': 'width: 300px;', 'class': 'form-control'}))
     number_of_people = forms.IntegerField(widget=forms.NumberInput(
-        attrs={'placeholder': 'Number of people', 'style': 'width: 300px;', 'class': 'form-control'}))
+        attrs={'min': 1, 'max': '8', 'type': 'number', 'placeholder': 'Number of people', 'style': 'width: 300px;', 'class': 'form-control'}))
+    table = forms.ChoiceField(choices=tableChoices, widget=forms.Select(
+        attrs={'style': 'width: 300px;', 'class': 'form-control'}))
     date = forms.DateField(widget=forms.NumberInput(
-        attrs={'placeholder': 'Which day do you wish to visit us?', 'class': 'form-control', 'type': 'date', 'style': 'width: 300px;'}))
+        attrs={'class': 'form-control', 'type': 'date', 'style': 'width: 300px;'}))
     time = forms.ChoiceField(choices=sittingTimes, widget=forms.Select(
-        attrs={'placeholder': 'What time do you want to visit us?', 'style': 'width: 300px;', 'class': 'form-control'}))
+        attrs={'style': 'width: 300px;', 'class': 'form-control'}))
 
     class Meta:
         model = Reservation
-        fields = ['name', 'email', 'phone_number', 'number_of_people', 'date', 'time']
+        fields = ['name', 'email', 'phone_number',
+                  'number_of_people', 'date', 'time', 'table']
+        unique_together = ('date', 'time', 'table',)
--- restaurant/models.py
+++ restaurant/models.py
@@ -7,10 +7,17 @@ from datetime import datetime
 # Create your models here.
 
 sittingTimes = (
-    ("3-5", "15-17"),
-    ("5-7", "17-19"),
-    ("7-9", "19-21"),
+    ("3-5", "3pm-5pm"),
+    ("5-7", "5pm-7pm"),
+    ("7-9", "7pm-9pm"),
 )
+
+tableChoices = [
+    ('1', 'Bigger table by window'),
+    ('2', 'Smaller table by window'),
+    ('3', 'Big table in isle'),
+    ('4', 'small table in isle')
+]
 
 
 class Reservation(models.Model):
@@ -19,10 +26,9 @@ class Reservation(models.Model):
     phone_number = models.IntegerField()
     number_of_people = models.IntegerField(validators=[MinValueValidator(0),
                                                        MaxValueValidator(5)])
-    table = models.IntegerField(null=True, blank=False, validators=[MinValueValidator(0),
-                                MaxValueValidator(5)])
-    date = datetime.now()
-    time = models.CharField(choices=sittingTimes, max_length=25)
+    table = models.TextField(choices=tableChoices, max_length=255)
+    date = models.DateTimeField(default=datetime.now)
+    time = models.TextField(choices=sittingTimes, max_length=255)
 
     def __str__(self):
         return self.name
