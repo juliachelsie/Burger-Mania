cca6ea8fc51e5ca88d1256b77a66a2eb59326057
juliachelsie <>
2024-01-29 11:27:41 +0000

    Trying to deploy

--- burgermania/settings.py
+++ burgermania/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['burgermania-julia.herokuapp.com', 'localhost',
                  '8000-juliachelsie-burger-mani-xvqq8chm6r.us2.codeanyapp.com', 'burgermania-julia-0c6fa3399ea8.herokuapp.com']
